0f6feaf9e8fbc18db9728165abea167d0974c62b
Mo Morsi <mo@morsi.org>
2019-05-02 02:13:38 -0400

    WIP: digest signing

--- lib/xrbp/crypto/key.rb
+++ lib/xrbp/crypto/key.rb
@@ -29,6 +29,7 @@ module XRBP
         ecpriv = OpenSSL::PKey::EC.new("secp256k1")
          ecpub = ecgrp.generator.mul(pk.to_bn)
 
+
         {  :public => ecpub.to_bn(:compressed).to_s(16),
           :private => priv.unpack("H*").first.upcase}
       end
@@ -46,6 +47,35 @@ module XRBP
         key = Ed25519::SigningKey.generate
         {  :public => key.to_bytes.unpack("H*").first.upcase,
           :private => key.verify_key.to_bytes.unpack("H*").first.upcase }
+      end
+
+      ###
+
+      def self.serialize_signature(sig)
+        return sig
+      end
+
+      def self.sign_digest(key, data)
+        raise "unknown key" unless key.is_a?(Hash) && key[:type]
+        raise "missing key" unless key[:public] && key[:private]
+
+        if key[:type] == :secp256k1
+          eckey = OpenSSL::PKey::EC.new("secp256k1")
+          ecgrp = OpenSSL::PKey::EC::Group.new('secp256k1')
+          eckey.public_key  = OpenSSL::PKey::EC::Point.new(ecgrp, OpenSSL::BN.new(key[:public],  16))
+          eckey.private_key = OpenSSL::BN.new(key[:private], 16)
+          #sha256 = OpenSSL::Digest::SHA256.new
+          #return eckey.sign(sha256, data)
+          return serialize_signature(eckey.dsa_sign_asn1(data))
+
+        #elsif key[:type] == :ed25519
+          # TODO
+        end
+
+        raise "unknown key type"
+      end
+
+      def self.verify(key, data, expected)
       end
     end
   end # module Crypto
