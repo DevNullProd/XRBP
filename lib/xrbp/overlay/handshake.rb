6c130b324de42f50d03f178c3ff7bfde6299fcc5
Mo Morsi <mo@morsi.org>
2019-08-29 11:26:03 -0400

    Use correct openssl finished_message, peer_finish_message methods

--- lib/xrbp/overlay/handshake.rb
+++ lib/xrbp/overlay/handshake.rb
@@ -12,8 +12,8 @@ module XRBP
     # XXX this module requires the openssl-ruby gem with the following patches:
     #     https://github.com/ruby/openssl/pull/250
     #
-    #     Otherwise the ssl_socket#finished and #peer_finished methods will
-    #     not be available
+    #     Otherwise the ssl_socket#finished_message and #peer_finished_message
+    #     methods will not be available
     #
     #     Currently the only way to apply this is to checkout openssl-ruby, apply
     #     the patches, and then rebuild/reinstall the gem locally!
@@ -39,8 +39,8 @@ module XRBP
       def shared
         @shared ||= begin
           sha512 = OpenSSL::Digest::SHA512.new
-          sf = socket.finished
-          pf = socket.peer_finished
+          sf = socket.finished_message
+          pf = socket.peer_finished_message
           sf = sha512.digest(sf)
           pf = sha512.digest(pf)
           shared = sf.to_bn ^ pf.to_bn
