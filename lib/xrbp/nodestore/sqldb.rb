ed34fc87feb18b1ef68a879adb4cd46ad40fc5e9
Mo Morsi <mo@morsi.org>
2019-10-18 16:26:43 -0400

    Stub-out nodestore sqlite3 db interface

---
+++ lib/xrbp/nodestore/sqldb.rb
@@ -0,0 +1,7 @@
+module XRBP
+  module NodeStore
+    class SQLDB
+      # TODO simple mechanisms to parse sqlite db rippled maintains for quick lookup mechanisms
+    end # class SQLDB
+  end # module NodeStore
+end # module XRBP
