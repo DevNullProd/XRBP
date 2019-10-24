4107c31f2addc4a87b055f9641594fee2d6a1f24
Mo Morsi <mo@morsi.org>
2019-10-24 14:25:30 -0400

    Add stubbed out spec file missing from previous commit

---
+++ spec/xrbp/nodestore/sle/st_amount_comparison_spec.rb
@@ -0,0 +1,11 @@
+describe XRBP::NodeStore::STAmount do
+  it "is < other amount"
+
+  it "is > other amount"
+
+  it "is >= other amount"
+
+  it "is == other amount"
+
+  it "is <=> other amount"
+end # describe XRBP::NodeStore::STAmount
